36c6c72c0de1ea9253e309eb958f23a5396cee0f
vm17399 <valentino.mascherini@students.uni-freiburg.de>
2022-01-27 11:12:46 +0100

    up

--- pear_by_tavg_did.R
+++ pear_by_tavg_did.R
@@ -2,38 +2,40 @@
 
 dtt <- as.data.frame(read_csv("https://raw.githubusercontent.com/vm17399/weather_api/main/api_data_weather.csv"))
 
-pointer <- read_csv("https://raw.githubusercontent.com/vm17399/weather_api/main/pointer.csv")
+pointer <- read_csv("https://raw.githubusercontent.com/vm17399/weather_api/main/pointer22.csv")
 
-hobos_did <- full_join(hobos_full, pointer, by = "id")
+#pointer22 <- point %>% filter(term_id == 13) %>% select(id, did) %>% mutate(did = as.integer(did))
+
+hobos_2022 <- read_csv("https://raw.githubusercontent.com/vm17399/weather_api/main/hobos_2022.csv")
+
+hobos_did <- full_join(hobos_2022, pointer, by = "id")
 
 hobos_did_avg <- hobos_did %>% group_by(did, dttm) %>%
   summarise(tavg = mean(th), did, dttm) %>% 
-  distinct_all()
+  distinct_all() %>% filter(is.na(tavg) == FALSE)
 
-write.csv(pointer, file = "~/Freiburg/UniFreiburg/Data Management/weather_api/pointer.csv", row.names = FALSE, quote = FALSE)
+write.csv(pointer22, file = "~/Freiburg/UniFreiburg/Data Management/weather_api/pointer22.csv", row.names = FALSE, quote = FALSE)
+
+
 
 #hobos_did_pear <- hobos_did_avg %>% group_by(did) %>% 
  # summarise(pears = cor.test(x = dev$th, y = dth$th, model ="pearson"))
 
-didder <- data.frame(did = as.integer(point$did)) %>% distinct_all() %>% filter(did != "integer(0)")
+didder <- data.frame(did = as.integer(pointer$did)) %>% 
+  distinct_all() %>% 
+  filter(did != "integer(0)")
 
 real <- data.frame(pear = NA, did = NA)
+
+dtg <- dtt
 
 j <- 1
 
 while (j < nrow(didder)) {
+ 
+  hda <- hobos_did_avg %>% filter(did == didder[[j,1]]) %>% slice(c(1:648))
   
-  hda <- hobos_did_avg %>% filter(did == didder[[j,1]]) 
-  
-  if(i == 1) {
-    
-    dtg <- dtt %>% slice(1:nrow(hda))
-  } else {
-    
-    dtg <- dtt %>% slice(1:nrow(hda))
-  }
-  
-  pearson <- cor.test(x = hda$th, y = dtg$th, model ="pearson")
+  pearson <- cor.test(x = hda$tavg, y = dtg$th, model ="pearson")
   
   real <- union(real, data.frame(pear = pearson$estimate, 
                                  did =  didder[[j, 1]]))
