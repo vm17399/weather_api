9d5b024820be5179701602416edfcc9eb0254cef
vm17399 <valentino.mascherini@students.uni-freiburg.de>
2022-01-27 10:43:49 +0100

    up

--- pear_by_tavg_did.R
+++ pear_by_tavg_did.R
@@ -1,7 +1,8 @@
+# get data frames
 
-read.csv()
+dtt <- as.data.frame(read_csv("https://raw.githubusercontent.com/vm17399/weather_api/main/api_data_weather.csv"))
 
-pointer <- point %>% select(id, did)
+pointer <- read_csv("https://raw.githubusercontent.com/vm17399/weather_api/main/pointer.csv")
 
 hobos_did <- full_join(hobos_full, pointer, by = "id")
 
@@ -9,19 +10,20 @@ hobos_did_avg <- hobos_did %>% group_by(did, dttm) %>%
   summarise(tavg = mean(th), did, dttm) %>% 
   distinct_all()
 
+write.csv(pointer, file = "~/Freiburg/UniFreiburg/Data Management/weather_api/pointer.csv", row.names = FALSE, quote = FALSE)
 
 #hobos_did_pear <- hobos_did_avg %>% group_by(did) %>% 
  # summarise(pears = cor.test(x = dev$th, y = dth$th, model ="pearson"))
 
 didder <- data.frame(did = as.integer(point$did)) %>% distinct_all() %>% filter(did != "integer(0)")
 
-dtt <- as.data.frame(read_csv("https://raw.githubusercontent.com/vm17399/weather_api/main/api_data_weather.csv"))
+real <- data.frame(pear = NA, did = NA)
 
 j <- 1
 
-while (j < nrow(didder) {
+while (j < nrow(didder)) {
   
-  hda <- hobos_did_avg %>% filter(did = didder[[j,1]]) 
+  hda <- hobos_did_avg %>% filter(did == didder[[j,1]]) 
   
   if(i == 1) {
     
@@ -34,5 +36,6 @@ while (j < nrow(didder) {
   pearson <- cor.test(x = hda$th, y = dtg$th, model ="pearson")
   
   real <- union(real, data.frame(pear = pearson$estimate, 
-                                 did =  didder[[j, 1]]) 
+                                 did =  didder[[j, 1]]))
+  j <- j + 1
 }
